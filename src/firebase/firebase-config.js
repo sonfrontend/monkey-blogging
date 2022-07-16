6b00dddb64e9bdaa8dbd18fee3a3c39250b1e890
Son <6051051100@st.utc2.edu.vn>
2022-07-16 11:54:34 +0700

    init project

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFiresStore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyBVgoxU-wKeb5846undFbBhyYAHDQ70zlE",
+  authDomain: "monkey-blogging-e042a.firebaseapp.com",
+  projectId: "monkey-blogging-e042a",
+  storageBucket: "monkey-blogging-e042a.appspot.com",
+  messagingSenderId: "610935837743",
+  appId: "1:610935837743:web:014dc0a226ceaddac322da",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFiresStore(app);
+
+export const auth = getAuth(app);
