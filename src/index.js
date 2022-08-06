a6ef8ebe2edb61a6dc356ed94ccd2f0cf09bb59d
Son <6051051100@st.utc2.edu.vn>
2022-08-06 15:46:08 +0700

    project configuration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
+import "./styles/index.scss";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { theme } from "./utils/constants";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider them={theme}>
+      <GlobalStyles></GlobalStyles>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
+
+reportWebVitals();
